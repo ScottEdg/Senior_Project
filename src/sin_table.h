7a0bf2db0141e1aaeedb2ade1a5dd12e8e3b4b0d
ScottEdg <scott.edgerly@maine.edu>
2015-10-20 23:17:45 -0400

    Working code that generates 3-Phase PWM (but needs to be tested) and added a sine Look Up Table to a header file.

---
+++ src/sin_table.h
@@ -0,0 +1,13 @@
+/*
+ * PWMInit.h
+ *
+ *  Created on: Oct 10, 2015
+ *      Author: Thor
+ */
+ #include <stdint.h>
+#ifndef SIN_TABLE_H_
+#define SIN_TABLE_H_
+
+extern const uint16_t sin_table[360];
+
+#endif /* SIN_TABLE_H_ */
