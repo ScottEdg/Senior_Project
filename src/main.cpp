9e183310d06269a1bed99c17dd730697a1e92396
Rigel Paradise <rigel.paradise@maine.edu>
2015-11-03 20:04:36 -0500

    Currently testing how duty cycle affects RPM of motor. Has: duty cycle variable, 45 90 360 lookup tables

--- src/main.cpp
+++ src/main.cpp
@@ -69,13 +69,31 @@ extern "C" void TIM4_IRQHandler(void)
     {
         if (__HAL_TIM_GET_ITSTATUS(&Tim4Handle, TIM_IT_UPDATE) != RESET)
         {
+        	double dooty = 0.4;
+            /*__HAL_TIM_CLEAR_FLAG(&Tim4Handle, TIM_FLAG_UPDATE);								//(These pin assignments should be double checked before use
+            __HAL_TIM_SET_COMPARE(&Tim1Handle,TIM_CHANNEL_1, uint32_t(dooty * sin_table[index]));				//Phase U+ on PA8
+            __HAL_TIM_SET_COMPARE(&Tim1Handle,TIM_CHANNEL_3, uint32_t(dooty * sin_table[(index+15)%30]));	//Phase U- on PA10
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_1, uint32_t(dooty * sin_table[(index+10)%30]));	//Phase V+ on PC6
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_2, uint32_t(dooty * sin_table[(index+25)%30]));	//Phase V- on PC7
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_3, uint32_t(dooty * sin_table[(index+20)%30]));	//Phase W+ on PC8
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_4, uint32_t(dooty * sin_table[(index+5)%30])); 	//Phase W- on PC9
+            */
             __HAL_TIM_CLEAR_FLAG(&Tim4Handle, TIM_FLAG_UPDATE);								//(These pin assignments should be double checked before use
-            __HAL_TIM_SET_COMPARE(&Tim1Handle,TIM_CHANNEL_1, sin_table[index]);				//Phase U+ on PA8
-            __HAL_TIM_SET_COMPARE(&Tim1Handle,TIM_CHANNEL_3, sin_table[(index+45)%90]);	//Phase U- on PA10
-            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_1, sin_table[(index+30)%90]);	//Phase V+ on PC6
-            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_2, sin_table[(index+75)%90]);	//Phase V- on PC7
-            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_3, sin_table[(index+60)%90]);	//Phase W+ on PC8
-            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_4, sin_table[(index+15)%90]); 	//Phase W- on PC9
+            __HAL_TIM_SET_COMPARE(&Tim1Handle,TIM_CHANNEL_1, uint32_t(dooty * sin_table2[index]));				//Phase U+ on PA8
+            __HAL_TIM_SET_COMPARE(&Tim1Handle,TIM_CHANNEL_3, uint32_t(dooty * sin_table2[(index+45)%90]));	//Phase U- on PA10
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_1, uint32_t(dooty * sin_table2[(index+30)%90]));	//Phase V+ on PC6
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_2, uint32_t(dooty * sin_table2[(index+75)%90]));	//Phase V- on PC7
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_3, uint32_t(dooty * sin_table2[(index+60)%90]));	//Phase W+ on PC8
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_4, uint32_t(dooty * sin_table2[(index+15)%90])); 	//Phase W- on PC9
+            /*
+            __HAL_TIM_CLEAR_FLAG(&Tim4Handle, TIM_FLAG_UPDATE);								//(These pin assignments should be double checked before use
+            __HAL_TIM_SET_COMPARE(&Tim1Handle,TIM_CHANNEL_1, uint32_t(dooty * sin_table2[index]));				//Phase U+ on PA8
+            __HAL_TIM_SET_COMPARE(&Tim1Handle,TIM_CHANNEL_3, uint32_t(dooty * sin_table2[(index+15)%30]));	//Phase U- on PA10
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_1, uint32_t(dooty * sin_table2[(index+10)%30]));	//Phase V+ on PC6
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_2, uint32_t(dooty * sin_table2[(index+25)%30]));	//Phase V- on PC7
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_3, uint32_t(dooty * sin_table2[(index+20)%30]));	//Phase W+ on PC8
+            __HAL_TIM_SET_COMPARE(&Tim8Handle,TIM_CHANNEL_4, uint32_t(dooty * sin_table2[(index+5)%30])); 	//Phase W- on PC9
+            */
             index++;
             index = index % 90; //reset index to 0 if we get to 360
         }
@@ -98,7 +116,7 @@ void PWM_Init(void)
 	HAL_GPIO_Init(GPIOA, &GPIO_BaseStruct);
 	// ------------------------------------------------------------------------
 
-	// --Set TIM1 to 10kHz switching frequency---------------------------------
+	// --Set TIM1 to 20kHz switching frequency---------------------------------
 	Tim1Handle.Instance = TIM1;
 	Tim1Handle.Init.Period = 8399;
 	Tim1Handle.Init.Prescaler = 0;
@@ -122,7 +140,7 @@ void PWM_Init(void)
 	HAL_GPIO_Init(GPIOC, &GPIO_BaseStruct); //Initalize the struct
 	// ------------------------------------------------------------------------
 
-	// --Set TIM8 to 10kHz switching frequency---------------------------------
+	// --Set TIM8 to 20kHz switching frequency---------------------------------
 	Tim8Handle.Instance = TIM8;
 	Tim8Handle.Init.Period = 8399;
 	Tim8Handle.Init.Prescaler = 0;
@@ -171,8 +189,8 @@ void TIM4_Init()
 	// --Set up TIM4-----------------------------------------------------------
 	Tim4Handle.Instance = TIM4;
 //TODO: Recalculate the Prescaler and create a range for the period to change the speed using ADC
-	Tim4Handle.Init.Period = 95; //This changes how fast to go through the sine LUT
-	Tim4Handle.Init.Prescaler = 104;
+	Tim4Handle.Init.Period = 380; //This changes how fast to go through the sine LUT
+	Tim4Handle.Init.Prescaler = 209;
 	Tim4Handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	Tim4Handle.Init.CounterMode = TIM_COUNTERMODE_UP;
 	Tim4Handle.State = HAL_TIM_STATE_RESET;
