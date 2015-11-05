86ce0b38e1853bf6e3d1ed2fdca9a194350afc11
Rigel Paradise <rigel.paradise@maine.edu>
2015-11-04 22:03:22 -0500

    Motor spins with start up. User button doubles speed. current and desired speed is implemented

--- src/sin_table.h
+++ src/sin_table.h
@@ -8,7 +8,6 @@
 #ifndef SIN_TABLE_H_
 #define SIN_TABLE_H_
 
-extern const uint16_t sin_table[30];
-extern const uint16_t sin_table2[90];
-extern const uint16_t sin_table3[360];
+extern const uint16_t sin_table[360];
+
 #endif /* SIN_TABLE_H_ */
